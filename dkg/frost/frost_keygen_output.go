54fcaf7210a341fe281b67e4ad42a7763b94ddd6
gldeng <realgldeng@gmail.com>
2022-11-03 15:39:03 +0800

    Refactor verifyShares

--- dkg/frost/frost_keygen_output.go
+++ dkg/frost/frost_keygen_output.go
@@ -1,6 +1,7 @@
 package frost
 
 import (
+	"bytes"
 	"github.com/bloxapp/ssv-spec/dkg"
 	"github.com/bloxapp/ssv-spec/types"
 	"github.com/herumi/bls-eth-go-binary/bls"
@@ -13,9 +14,12 @@ func (fr *FROST) processKeygenOutput() (*dkg.KeyGenOutput, error) {
 		return nil, nil
 	}
 
-	if _, err := fr.verifyShares(); err != nil {
+	reconstructed, err := fr.verifyShares()
+	if err != nil {
 		return nil, errors.Wrap(err, "failed to verify shares")
 	}
+
+	reconstructedBytes := reconstructed.Serialize()
 
 	out := &dkg.KeyGenOutput{
 		Threshold: uint64(fr.state.threshold),
@@ -47,52 +51,83 @@ func (fr *FROST) processKeygenOutput() (*dkg.KeyGenOutput, error) {
 	}
 
 	out.OperatorPubKeys = operatorPubKeys
+
+	if !bytes.Equal(out.ValidatorPK, reconstructedBytes) {
+		return nil, errors.New("can't reconstruct to the validator pk")
+	}
+
 	return out, nil
 }
 
-func (fr *FROST) verifyShares() ([]*bls.G1, error) {
+func (fr *FROST) verifyShares() (*bls.G1, error) {
 
-	outputs := make([]*bls.G1, 0)
+	var (
+		quorumStart       = 0
+		quorumEnd         = int(fr.state.threshold)
+		prevReconstructed = (*bls.G1)(nil)
+	)
 
-	for j := int(fr.state.threshold); j < len(fr.state.operators); j++ {
-
-		xVec := make([]bls.Fr, 0)
-		yVec := make([]bls.G1, 0)
-
-		for i := j - int(fr.state.threshold); i < j; i++ {
-			operatorID := fr.state.operators[i]
-
-			protocolMessage := &ProtocolMsg{}
-			if err := protocolMessage.Decode(fr.state.msgs[Round2][operatorID].Message.Data); err != nil {
-				return nil, errors.Wrap(err, "failed to decode protocol msg")
-			}
-
-			x := bls.Fr{}
-			x.SetInt64(int64(operatorID))
-			xVec = append(xVec, x)
-
-			pk := &bls.PublicKey{}
-			if err := pk.Deserialize(protocolMessage.Round2Message.VkShare); err != nil {
-				return nil, err
-			}
-
-			y := bls.CastFromPublicKey(pk)
-			yVec = append(yVec, *y)
-		}
-
-		out := &bls.G1{}
-		if err := bls.G1LagrangeInterpolation(out, xVec, yVec); err != nil {
+	// Sliding window of quorum 0...threshold until n-threshold...n
+	for quorumEnd < len(fr.state.operators) {
+		quorum := fr.state.operators[quorumStart:quorumEnd]
+		currReconstructed, err := fr.verifyShare(quorum)
+		if err != nil {
 			return nil, err
 		}
-
-		outputs = append(outputs, out)
-	}
-
-	for i := 1; i < len(outputs); i++ {
-		if !outputs[i].IsEqual(outputs[i-1]) {
+		if prevReconstructed != nil && !currReconstructed.IsEqual(prevReconstructed) {
 			return nil, errors.New("failed to create consistent public key from tshares")
 		}
+		prevReconstructed = currReconstructed
+		quorumStart++
+		quorumEnd++
+	}
+	return prevReconstructed, nil
+}
+
+func (fr *FROST) verifyShare(operators []uint32) (*bls.G1, error) {
+	xVec, err := fr.getXVec(operators)
+	if err != nil {
+		return nil, err
 	}
 
-	return outputs, nil
+	yVec, err := fr.getYVec(operators)
+	if err != nil {
+		return nil, err
+	}
+
+	reconstructed := &bls.G1{}
+	if err := bls.G1LagrangeInterpolation(reconstructed, xVec, yVec); err != nil {
+		return nil, err
+	}
+	return reconstructed, nil
+}
+
+func (fr *FROST) getXVec(operators []uint32) ([]bls.Fr, error) {
+	xVec := make([]bls.Fr, 0)
+	for _, operator := range operators {
+		x := bls.Fr{}
+		x.SetInt64(int64(operator))
+		xVec = append(xVec, x)
+	}
+	return xVec, nil
+}
+
+func (fr *FROST) getYVec(operators []uint32) ([]bls.G1, error) {
+	yVec := make([]bls.G1, 0)
+	for _, operator := range operators {
+
+		protocolMessage := &ProtocolMsg{}
+		if err := protocolMessage.Decode(fr.state.msgs[Round2][operator].Message.Data); err != nil {
+			return nil, errors.Wrap(err, "failed to decode protocol msg")
+		}
+
+		pk := &bls.PublicKey{}
+		if err := pk.Deserialize(protocolMessage.Round2Message.VkShare); err != nil {
+			return nil, errors.Wrap(err, "failed to deserialize public key")
+		}
+
+		y := bls.CastFromPublicKey(pk)
+		yVec = append(yVec, *y)
+	}
+	return yVec, nil
 }
